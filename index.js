8800da346dd5e9a5cf563039af7f6e5456c486cd
Jeffrey Davidsz <jeffrey.davidsz@vicreo.eu>
2023-02-24 22:13:57 +0100

    add announcements

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ class ProPresenter {
         throw new Error("Something went wrong");
       })
       .then((responseJson) => {
+        responseJson.success = path;
         return responseJson;
       })
       .catch((error) => {
@@ -50,8 +51,8 @@ class ProPresenter {
    * Requests the status of all available layers.
    * @returns Status of all available layers.
    */
-  async statusLayers() {
-    return await this.fetchResource("/v1/status/layers", []);
+  statusLayers() {
+    return this.fetchResource("/v1/status/layers", []);
   }
 
   /**
@@ -60,6 +61,82 @@ class ProPresenter {
    */
   version() {
     return this.getDataFromProPresenter("/version", []);
+  }
+
+  /**
+   * ANNOUNCEMENT
+   */
+
+  /**
+   * Requests the currently active announcement presentation.
+   * @returns the currently active announcement presentation.
+   */
+  announcementActive() {
+    return this.getDataFromProPresenter("/v1/announcement/active", []);
+  }
+  /**
+   * Requests the index of the current slide/cue within the currently active announcement.
+   * @returns The index of the current slide/cue within the currently active announcement.
+   */
+  announcementSlideIndex() {
+    return this.getDataFromProPresenter("/v1/announcement/slide_index", []);
+  }
+  /**
+   * Focuses the currently active announcement presentation.
+   */
+  announcementActiveFocus() {
+    return this.getDataFromProPresenter("/v1/announcement/active/focus", []);
+  }
+  /**
+   * Retriggers the currently active announcement presentation (starts from the beginning).
+   */
+  announcementTrigger() {
+    return this.getDataFromProPresenter("/v1/announcement/active/trigger", []);
+  }
+  /**
+   * Triggers the next cue in the active announcement presentation (if there is one).
+   */
+  announcementNextTrigger() {
+    return this.getDataFromProPresenter(
+      "/v1/announcement/active/next/trigger",
+      []
+    );
+  }
+  /**
+   * Triggers the previous cue in the currently active announcement presentation (if there is one).
+   */
+  announcementPreviousTrigger() {
+    return this.getDataFromProPresenter(
+      "/v1/announcement/active/previous/trigger",
+      []
+    );
+  }
+  /**
+   * Triggers the specified cue in the currently active announcement presentation.
+   * @param {string} index
+   */
+  announcementActiveIndexTrigger(index) {
+    return this.getDataFromProPresenter(
+      `/v1/announcement/active/${index}/trigger`,
+      []
+    );
+  }
+  /**
+   * Performs the requested timeline operation for the active announcment presentation.
+   * @param {play,pause,rewind} operation
+   */
+  announcementActiveTimelineOperation(operation) {
+    return this.getDataFromProPresenter(
+      `/v1/announcement/active/timeline/${operation}`,
+      []
+    );
+  }
+  /**
+   * Requests the current state of the active announcement timeline.
+   * @returns The current state of the active announcement timeline.
+   */
+  announcementActiveTimelineOperation() {
+    return this.getDataFromProPresenter(`/v1/announcement/active/timeline`, []);
   }
 }
 
