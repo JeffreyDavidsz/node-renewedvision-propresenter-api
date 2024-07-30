81f0e1a3b243f125d35dcfe436cf2de5dee267cb
daniel owen <greyshirtguy@gmail.com>
2024-07-30 20:47:06 +1000

    Added user-defined timeout for all network fetch operations. If not explicitly set, this module will default to 2 seconds. This default is much shorter than the Node default fetch timeout of 30 seconds, making it more suitable for remote-control situations.

--- src/propresenter.ts
+++ src/propresenter.ts
@@ -1,18 +1,20 @@
-export type JSONValue =
+export type JSONValue = // TODO: Perhaps JSONResponseAndPath is a better name?
   | {
       data: any;
       status: number;
-      command: string;
+      command: string; // TODO: Perhaps path is a better name?
     }
   | Promise<JSONValue>;
 
 export class ProPresenter {
-  ip: string;
+  ip: string; // TODO: Perhaps host is a better name? (As either a hostname or IP string would work)
   port: number;
+  timeout: number; // User-defined timeout for all network fetch operations. If not explicitly set, this module will default to 2 seconds. This default is much shorter than the Node default fetch timeout of 30 seconds, making it more suitable for remote-control situations.
 
-  constructor(ip: string, port: number) {
+  constructor(ip: string, port: number, timeout: number = 2000) {
     this.ip = ip;
     this.port = port;
+    this.timeout = timeout;
   }
 
   //
@@ -23,6 +25,9 @@ export class ProPresenter {
    * @returns Promise from fetch
    */
   private sendRequestToProPresenter = (path: string, userOptions?: any) => {
+    // AbortController - used for user-defined timeout
+    const abortController = new AbortController();
+    
     // Define default options
     const defaultOptions = {};
     // Define default headers
@@ -33,6 +38,7 @@ export class ProPresenter {
       // Merge options
       ...defaultOptions,
       ...userOptions,
+      signal: abortController.signal, // Used to abort fetch, in case of no reponse within user-defined timeout
       // Merge headers
       headers: {
         ...defaultHeaders,
@@ -43,8 +49,13 @@ export class ProPresenter {
     // Build Url
     const url = `http://${this.ip}:${this.port}${path}`;
     const resultObj: JSONValue = { data: null, status: null, command: path };
+
+    // Prepare to abort fetch if not completed within user-defined timeout
+    const timeoutId = setTimeout(() => abortController.abort(), this.timeout);
+
     return fetch(url, options)
       .then((response) => {
+        clearTimeout(timeoutId); // Response received - clear pending abort for user-defined timeout
         resultObj.status = response.status;
         const contentType = response.headers.get("content-type");
         if (contentType && contentType.indexOf("application/json") !== -1) // Check if response from Pro7 contains a JSON body. Some Pro7 API GET requests (eg most /trigger's) return only a header without any response body
